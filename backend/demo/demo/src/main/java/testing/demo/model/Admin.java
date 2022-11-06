eadaa70fa52b25a82594cb76fe93c87680e47c72
tshiamo-masilo <63866003+tshiamo-masilo@users.noreply.github.com>
2022-11-06 12:46:37 +0200

    Update on the model and Repository files with use of Lomok

--- backend/demo/demo/src/main/java/testing/demo/model/Admin.java
+++ backend/demo/demo/src/main/java/testing/demo/model/Admin.java
@@ -1,6 +1,8 @@
 package testing.demo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.*;
@@ -9,11 +11,13 @@ import java.io.Serializable;
 @Data
 @Entity
 @Table(name ="AdminTable")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Admin implements Serializable {
     @Id
     @GeneratedValue()
     @Column()
-    private int id;
+    private Long AdminId;
     private String Full_names;
     private int School_id;
     private String Email;
@@ -21,96 +25,4 @@ public class Admin implements Serializable {
     private String Phone_Number;
     private String Password;
     private String Employee_no;
-     public Admin(){
-
-     }
-
-    public Admin(String full_names, int school_id, String email, String username, String number, String password, String employee_no) {
-        Full_names = full_names;
-        School_id = school_id;
-        Email = email;
-        Username = username;
-        Phone_Number = number;
-        Password = password;
-        Employee_no = employee_no;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFull_names() {
-        return Full_names;
-    }
-
-    public void setFull_names(String full_names) {
-        Full_names = full_names;
-    }
-
-    public int getSchool_id() {
-        return School_id;
-    }
-
-    public void setSchool_id(int school_id) {
-        School_id = school_id;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public String getUsername() {
-        return Username;
-    }
-
-    public void setUsername(String username) {
-        Username = username;
-    }
-
-    public String getNumber() {
-        return Phone_Number;
-    }
-
-    public void setNumber(String number) {
-        Phone_Number = number;
-
-    }
-
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
-    }
-
-    public String getEmployee_no() {
-        return Employee_no;
-    }
-
-    public void setEmployee_no(String employee_no) {
-        Employee_no = employee_no;
-    }
-
-    @Override
-    public String toString() {
-        return "Admin{" +
-                "id=" + id +
-                ", Full_names='" + Full_names + '\'' +
-                ", School_id=" + School_id +
-                ", Email='" + Email + '\'' +
-                ", Username='" + Username + '\'' +
-                ", Phone_Number='" + Phone_Number + '\'' +
-                ", Password='" + Password + '\'' +
-                ", Employee_no='" + Employee_no + '\'' +
-                '}';
-    }
 }
--- backend/demo/demo/src/main/java/testing/demo/model/UserCredentials.java
+++ backend/demo/demo/src/main/java/testing/demo/model/UserCredentials.java
@@ -1,7 +1,9 @@
 package testing.demo.model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -9,12 +11,14 @@ import java.io.Serializable;
 @Data
 @Entity
 @Table(name="RegisterDetails")
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCredentials {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column( nullable = false, updatable = true, unique = true)
-    private int id;
+    @Column(nullable = false, updatable = true, unique = true)
+    private Long UserId;
     private String Name;
     private String Surname;
     private String Email;
@@ -22,86 +26,4 @@ public class UserCredentials {
     private String Phone_Number;
     private String Password;
 
-
-    public UserCredentials(){
-
-    }
-    public UserCredentials(String Name, String surname,String Username,
-   String Number,
-     String Password){
-        this.Name = Name;
-        this.Surname = surname;
-        this.Username = Username;
-        this.Phone_Number = Number;
-        this.Password = Password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public String getSurname() {
-        return Surname;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public void setSurname(String surname) {
-        Surname = surname;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public String getUsername() {
-        return Username;
-    }
-
-    public void setUsername(String username) {
-        Username = username;
-    }
-
-    public String getNumber() {
-        return Phone_Number;
-    }
-
-    public void setNumber(String number) {
-        Phone_Number = number;
-    }
-
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "UserCredentials{" +
-                "id=" + id +
-                ", Name='" + Name + '\'' +
-                ", Surname='" + Surname + '\'' +
-                ", Email='" + Email + '\'' +
-                ", Username='" + Username + '\'' +
-                ", Number='" + Phone_Number + '\'' +
-                ", Password='" + Password + '\'' +
-                '}';
-    }
 }
