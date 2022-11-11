a23478879677f15e95836afc42d881e652398c48
lebohangtamako <114900748+lebohangtamako@users.noreply.github.com>
2022-11-11 08:33:17 +0200

    committing user story 24

--- backend/demo/demo/src/main/java/testing/demo/Repository/GuardianRepo.java
+++
@@ -1,7 +0,0 @@
-package testing.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import testing.demo.model.Guardian;
-
-public interface GuardianRepo extends JpaRepository<Guardian, Integer> {
-}
