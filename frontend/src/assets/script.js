23ad009eee9e87fa6c144ac85a991d8914e912bb
Yuvi <yeargambram@gmail.com>
2022-11-14 05:25:22 +0200

    Search bar update

--- frontend/src/assets/script.js
+++ frontend/src/assets/script.js
@@ -1,5 +1,38 @@
 function myFunction() { 
     document.getElementById("icon").style.display="none";  
     document.getElementById("filter").style.display="block";
-    document.getElementById("sections").style.display="flex";
+    document.getElementById("sections").style.display="block";
   }
+
+  var x = document.getElementById("demo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+
+//SEARCH BAR
+ 
+function search_schools() {
+  let input = document.getElementById('searchbar').value
+  input=input.toLowerCase();
+  let x = document.getElementsByClassName('schools');
+    
+  for (i = 0; i < x.length; i++) { 
+      if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].style.display="none";
+      }
+      else {
+          x[i].style.display="inherit";                 
+      }
+  }
+}
